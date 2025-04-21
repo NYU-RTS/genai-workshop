1c77358fdaca2ec6af8173fe4552e391cbb0ef71
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2025-04-21 15:41:46 -0400

    add chat_image.R to demo sending image files, add deps and make pixi env cross-platform

---
+++ R/chat_image.R
@@ -0,0 +1,25 @@
+library(ellmer)
+
+if (Sys.getenv("API_KEY") == "") {
+  stop(paste("Error: Environment variable", API_KEY, "is not set!"))
+} else {
+  API_KEY <- Sys.getenv("API_KEY")
+}
+
+if (Sys.getenv("LLM_MODEL") == "") {
+  stop(paste("Error: Environment variable", API_KEY, "is not set!"))
+} else {
+  LLM_MODEL <- Sys.getenv("LLM_MODEL")
+}
+
+chat  <- chat_openai(
+  base_url="https://ai-gateway.apps.cloud.rt.nyu.edu/v1/",
+  api_key=API_KEY,
+  model=LLM_MODEL)
+
+PWD <- getwd()
+
+chat$chat(
+  "What do you see in these images?",
+  content_image_file(file.path(PWD,"images","Gfp-wisconsin-madison-the-nature-boardwalk.jpg"))
+)
