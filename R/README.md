cc2937c18cc4a7f34163b290da6685997632e4a8
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2025-05-21 15:48:53 -0400

    R: bump ellmer to latest release to use chat_portkey directly!

--- R/README.md
+++ R/README.md
@@ -7,3 +7,6 @@ If you do not already have R (or RStudio) installed, you can use the pixi pacakg
 
 ### Install R pacakges via renv
 Install the required depencies via renv (install renv first!). Here is a reference for renv: https://rstudio.github.io/renv/articles/renv.html
+
+### Grab your `API_KEY`, `VIRTUAL_KEY` and define the `LLM_MODEL`
+You can get these from the Portkey UI at `app.portkey.ai`. Set these as environment variables and run the scripts.
--- R/chat.R
+++ R/chat.R
@@ -6,15 +6,22 @@ if (Sys.getenv("API_KEY") == "") {
   API_KEY <- Sys.getenv("API_KEY")
 }
 
+if (Sys.getenv("VIRTUAL_KEY") == "") {
+  stop(paste("Error: Environment variable", VIRTUAL_KEY, "is not set!"))
+} else {
+  VIRTUAL_KEY <- Sys.getenv("VIRTUAL_KEY")
+}
+
 if (Sys.getenv("LLM_MODEL") == "") {
   stop(paste("Error: Environment variable", API_KEY, "is not set!"))
 } else {
   LLM_MODEL <- Sys.getenv("LLM_MODEL")
 }
 
-chat <- chat_openai(
+chat <- chat_portkey(
   base_url = "https://ai-gateway.apps.cloud.rt.nyu.edu/v1/",
   api_key = API_KEY,
+  virtual_key = VIRTUAL_KEY,
   model = LLM_MODEL
 )
 
